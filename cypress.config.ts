5e6fe80b00b636bcb6e475ffb5a590ff3dbf21e3
AVONALE\anderson.rocha <anderson.rocha@fandi.com.br>
2024-03-05 19:53:57 -0300

    Test add mochawesome-repoter

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,10 +1,18 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: 'Blog do Agi',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+  },
   e2e: {
     baseUrl: 'https://blogdoagi.com.br/',
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
 });
